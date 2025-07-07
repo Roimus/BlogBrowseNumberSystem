8642d5e7cc22cf3514120f425efa898095fad081
洪胜杰 <1436512909@qq.com>
2025-07-08 03:35:13 +0800

    增加文章表

--- BlogBrowseNumberSystem/settings.py
+++ BlogBrowseNumberSystem/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'BlogBrowseNumberSystem.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Blog',
+        'NAME': 'blog',
         'USER': 'root',
         'PASSWORD': '123456',
         'HOST': 'localhost',
