bc17115e68b89fe8ea39788c85b923b4fba74538
洪胜杰 <1436512909@qq.com>
2025-07-08 04:25:22 +0800

    增加视图和url

--- article/views.py
+++ article/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from service.services import ViewCountService
+from models import Article
 
-# Create your views here.
+
+def article_add(request, id):
+    article = get_object_or_404(Article, pk=id)
+
+    service = ViewCountService()
+    number = service.add_number(id)
+
+    return JsonResponse({
+        'title': article.title,
+        'content': article.content,
+        'number': number
+    })
